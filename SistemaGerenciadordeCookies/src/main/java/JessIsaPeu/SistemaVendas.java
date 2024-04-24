660c6a14768079e547d93e9ee6354505f548d761
jessiquinha19 <154331640+jessiquinha19@users.noreply.github.com>
2024-04-23 21:20:18 -0300

    Update SistemaVendas.java

--- SistemaGerenciadordeCookies/src/main/java/JessIsaPeu/SistemaVendas.java
+++ SistemaGerenciadordeCookies/src/main/java/JessIsaPeu/SistemaVendas.java
@@ -3,12 +3,12 @@ package JessIsaPeu;
 import java.util.List;
 
 public interface SistemaVendas {
-    void cadastraCookies(Cookie cookie) throws CookieJaExisteException;
+    void cadastraCookies(String sabor, double preco, TipoCookie tipo) throws CookieJaExisteException;
     List<Cookie> pesquisaCookiesPorSabor(String sabor) throws CookieNaoExisteException;
     boolean existeSabor(String sabor);
     int contaCookiesDoTipo(TipoCookie tipo);
-    void removeSabor(Cookie cookie) throws CookieNaoExisteException;
-    List<Cookie> pesquisaPorPreco(double preco);
+    void removeSabor(String sabor) throws CookieNaoExisteException;
+    List<Cookie> pesquisaValoresPorFaixa(double valorMinimo, double valorMaximo);
     boolean existeCookiesDoTipo(TipoCookie tipo);
-    List<Cookie> pesquisaCombos(String combo);
+    List<Combos> pesquisaCombos(String combo);
 }
